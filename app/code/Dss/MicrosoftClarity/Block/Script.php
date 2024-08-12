8b45cb3e7305655b21bc922e82b202d47954a968
Harsh <harshb@digitsoftsol.com>
2024-08-12 17:20:29 +0530

    Fix Coding standard and made module compatable with php version ^8.1

---
+++ app/code/Dss/MicrosoftClarity/Block/Script.php
@@ -0,0 +1,117 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+* Digit Software Solutions.
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the EULA
+* that is bundled with this package in the file LICENSE.txt.
+*
+* @category  Dss
+* @package   Dss_MicrosoftClarity
+* @author    Extension Team
+* @copyright Copyright (c) 2024 Digit Software Solutions. ( https://digitsoftsol.com )
+*/
+
+namespace Dss\MicrosoftClarity\Block;
+
+use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+use Magento\Customer\Model\Session as CustomerSession;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Customer\Model\ResourceModel\GroupRepository as CustomerGroupRepository;
+
+class Script extends Template
+{
+    /**
+     * Feature enabled config path
+     */
+    public const XML_MICROSOFT_CLARITY_GENERAL_ENABLE = 'microsoft_clarity/general/enable';
+
+    /**
+     * Customer group
+     */
+    public const MICROSOFT_CLARITY_CUSTOMER_GROUP_TAG_ENABLE = 'microsoft_clarity/general/customer_group_tag_enable';
+
+    /**
+     * Tracking code config path
+     */
+    public const XML_MICROSOFT_CLARITY_OPTIONS_TRACKING_CODE = 'microsoft_clarity/options/tracking_code';
+
+    /**
+     * Script constructor.
+     *
+     * @param \Magento\Framework\View\Element\Template\Context $context
+     * @param \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Customer\Model\ResourceModel\GroupRepository $customerGroupRepository
+     * @param array $data
+     */
+    public function __construct(
+        Context $context,
+        protected ScopeConfigInterface $scopeConfig,
+        protected CustomerSession $customerSession,
+        protected CustomerGroupRepository $customerGroupRepository,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * Returns the Clarity tracking code to be used in the javascript
+     *
+     * @return null|string
+     */
+    public function getClarityTrackingCode(): ?string
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_MICROSOFT_CLARITY_OPTIONS_TRACKING_CODE,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Checks if feature is enabled
+     *
+     * @return bool|string
+     */
+    public function isEnabled(): bool|string
+    {
+        return $this->scopeConfig->getValue(
+            self::XML_MICROSOFT_CLARITY_GENERAL_ENABLE,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Checks if customer group tag to be added
+     *
+     * @return bool|string
+     */
+    public function isCustomerGroupTagEnabled(): bool|string
+    {
+        return $this->scopeConfig->getValue(
+            self::MICROSOFT_CLARITY_CUSTOMER_GROUP_TAG_ENABLE,
+            ScopeInterface::SCOPE_STORE
+        );
+    }
+
+    /**
+     * Get Customer Groups
+     *
+     * @return string
+     * @throws \Magento\Framework\Exception\LocalizedException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     */
+    public function getCustomerGroup(): string
+    {
+        $customerGroupId = $this->customerSession->getCustomerGroupId();
+        $customerGroup = $this->customerGroupRepository->getById($customerGroupId);
+
+        return $customerGroup->getCode();
+    }
+}
--- app/code/Dss/MicrosoftClarity/README.md
+++ app/code/Dss/MicrosoftClarity/README.md
@@ -46,31 +46,31 @@ php bin/magento module:disable Dss_MicrosoftClarity
 
 ## Microsoft Clarity Tracking Code
 
-Once you have created a new project in [**Microsoft Clarity**](https://clarity.microsoft.com/), from your Project Dashboard, select the **Setup** tab. In the Setup tab, you will find the **Clarity tracking code**, and below that the script will be available.
+Once you have created a new project in [**Microsoft Clarity**](https://clarity.microsoft.com/), from your Project Dashboard, select the **Setup** tab. In the Setup tab under setting tab, you will find the **Clarity tracking code**, and below that the script will be available.
  
 We only need the tracking code to configure and enable Microsoft Clarity on your Magento website from the script.
-[![](app/code/Dss/MicrosoftClarity/docs/Tracking-code-2.png)](app/code/Dss/MicrosoftClarity/docs/Tracking-code-2.png)
+[![](docs/Tracking-code-2.png)](docs/Tracking-code-2.png)
 
 ## Configuration
 
 Path: **Stores → Configuration → DSS → Microsoft Clarity**
 
-[![](app/code/Dss/MicrosoftClarity/docs/Configuration.png)](app/code/Dss/MicrosoftClarity/docs/Configuration.png)
+[![](docs/Configuration.png)](docs/Configuration.png)
  
 - **Enabled:** _Yes/No_ - Sets whether the module is enabled or disabled. 
-- **Tracking Code:** _Text_ - Please enter your Microsoft Clarity tracking code here.
+- **Tracking Code:** - Please enter your Microsoft Clarity tracking code here.
 - **Enable Customer Group Tag** - If this is enabled, custom tags and filters will be created based on the Customer Groups available in Magento in Microsoft Clarity. So for each session and page that are tracked in Clarity, they will have a Customer Group tag, so it will be easier to filter and find session/recording for each page based on customer group.
 
 ## How the module works
  
 Once you have added the **Tracking Code**, the Microsoft Clarity Script will start to appear on the front end of each page. You can view this in Page Source or your browser's developer tools.
  
-[![](app/code/Dss/MicrosoftClarity/docs/console.png)](app/code/Dss/MicrosoftClarity/docs/console.png)
+[![](docs/console.png)](docs/console.png)
  
 Also, in your [Microsoft Clarity](https://clarity.microsoft.com/) Account, under the project you have created, you will start to see analytics details based on visitors' activity on your site. You can see this in:
  
 Log in to Your Microsoft Clarity Account, → Select your Project → Dashboard & Recording
  
-[![](app/code/Dss/MicrosoftClarity/docs/dashboard.png)](app/code/Dss/MicrosoftClarity/docs/dashboard.png)
+[![](docs/dashboard.png)](docs/dashboard.png)
 
 THANKS FOR CHOOSING DSS 
